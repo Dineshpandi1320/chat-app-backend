6d3183a2ee5d196bd542766771e05059834b041e
DineshPandi1320 <dineshd1307@gmail.com>
2023-10-05 10:35:42 +0530

    new updated

--- index.js
+++ index.js
@@ -1,48 +1,95 @@
 const express = require("express");
-const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
- 
-// Specify the allowed origin (your frontend domain)
-const allowedOrigin = "https://genuine-kringle-8bfe0a.netlify.app"; // Replace with your frontend domain
- 
-app.use(
-  cors({
-    origin: allowedOrigin, // Allow requests only from your frontend domain
-    methods: ["GET", "POST"],
-    credentials: true, // Enable credentials (cookies, authorization headers)
-  })
-);
- 
-const PORT = process.env.PORT || 3002;
+
+const app = express();
 const server = http.createServer(app);
- 
 const io = new Server(server, {
   cors: {
-    origin: allowedOrigin, // Allow WebSocket connections from your frontend domain
+    origin: "*",
     methods: ["GET", "POST"],
+    credentials: true,
   },
 });
- 
+
+// Store registered users' usernames and socket IDs in memory
+const registeredUsernames = {};
+const socketIdToUsername = {};
+
+// Store messages in memory
+const messages = [];
+
+function emitActiveUsers() {
+  const activeUserNames = Object.values(registeredUsernames);
+  io.emit("activeUsers", activeUserNames);
+  io.emit("Allmessages", messages)
+}
+
+// Define socket.io event listeners
 io.on("connection", (socket) => {
-  console.log(`User_connected ${socket.id}`);
- 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  console.log("User connected:", socket.id);
+
+  // Emit initial messages and active users to the connected client
+  socket.emit("messages", messages);
+  emitActiveUsers();
+
+  // Handle new messages
+  socket.on("message", (message) => {
+    // Add message to the array
+    messages.push({
+      message,
+      userId: socket.id,
+      userName: registeredUsernames[socket.id],
+    });
+    // Emit the updated messages to all connected clients
+    io.emit("message", {
+      message,
+      userId: socket.id,
+      userName: registeredUsernames[socket.id],
+    });
   });
- 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("received_message", data);
+
+  // Handle user registration
+  socket.on("register", (userName, callback) => {
+    if (
+      !socketIdToUsername[socket.id] &&
+      !Object.values(registeredUsernames).includes(userName)
+    ) {
+      console.log("User registered:", userName);
+      registeredUsernames[socket.id] = userName;
+      socketIdToUsername[socket.id] = userName;
+      emitActiveUsers();
+      callback({ success: true });
+    } else {
+      console.log("User registration failed:", userName);
+      callback({
+        success: false,
+        message: "Username already taken. Please try another.",
+      });
+    }
   });
- 
+
+  // Handle disconnection
   socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
+    console.log("User disconnected:", socket.id);
+    // Remove the user from the registered users object
+    const userName = socketIdToUsername[socket.id];
+    delete registeredUsernames[socket.id];
+    delete socketIdToUsername[socket.id];
+    emitActiveUsers();
   });
 });
- 
-server.listen(PORT, () => {
-  console.log("SERVER RUNNING");
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Start the server
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
- 
+
+
+
+
